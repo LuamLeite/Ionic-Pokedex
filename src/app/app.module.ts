292f7bc5bf53d4f820bda09cc23db4f3da4184c7
luamleite <luamleite@nasajon.com.br>
2020-09-15 21:02:23 -0300

    Commit inicial - criando componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { PokemonsComponent } from './pokemons/pokemons.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PokemonsComponent
   ],
   imports: [
     BrowserModule
